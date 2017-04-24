b391f4f76d0d7b7ae6cbb73c502965d6267f5a6c
alxhghs <alxhghs@gmail.com>
2017-04-23 21:11:39 -0400

    creating a function to show the categories

--- rango/urls.py
+++ rango/urls.py
@@ -7,4 +7,5 @@ from rango import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/', views.about, name='about'),
+    url(r'^category/', views.show_category, name='show_category'),
 ]
