5093acc4c9122de2d0b9d4031b4655c312a5a98a
Alex <alxhghs@gmail.com>
2017-04-29 09:43:27 -0400

    working on forms for add_page feature

--- rango/urls.py
+++ rango/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category,
         name='show_category'),
     url(r'^add_category/$', views.add_category, name='add_category'),
+    url(r'^add_page/(?P<category_name_slug>[\w\-]+)/$', views.add_page,
+        name='add_page'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,7 @@
 
 from django.shortcuts import render
 from rango.models import Category, Page
-from rango.forms import CategoryForm
+from rango.forms import CategoryForm, PageForm
 
 
 def index(request):
@@ -90,3 +90,25 @@ def add_category(request):
     print("views.add_category returned render b/c request.method != 'POST' "
           "or there are errors")
     return render(request, 'rango/add_category.html', {'form': form})
+
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+    form = PageForm()
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+                return show_category(request, category_name_slug)
+        else:
+            print(form.errors)
+
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context_dict)
